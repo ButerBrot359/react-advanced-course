9254326df180caea006c0ec8341de29aea3c9621
Aray Muratova <kanatkyzy98@gmail.com>
2023-08-29 15:05:26 +0600

    add: css variables and theme logic

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import { Route, Routes, Link } from "react-router-dom";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 
-import "./index.scss";
+import "./styles/index.scss";
 import { Suspense } from "react";
+import { useTheme } from "./theme/useTheme";
 
 export const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>TOGGLE THEME</button>
       <Link to={"/"}>Main Page</Link>
       <Link to={"/about"}>About Page</Link>
       <Suspense fallback={<div>Loading...</div>}>
