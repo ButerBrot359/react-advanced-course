311807a0f668d414aeca761ba7be36d6378f2eb6
buterbrot359 <batowka359@gmail.com>
2023-10-19 11:05:58 +0600

    add: Navbar

--- src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,7 +1,10 @@
 import { FC } from "react";
 import { classNames } from "shared/lib/classNames/classNames";
 import cls from "./ThemeSwitcher.module.scss";
-import { useTheme } from "app/providers/ThemeProvider";
+import { useTheme, Theme } from "app/providers/ThemeProvider";
+import LightIcon from "shared/assets/icons/theme-light.svg";
+import DarkIcon from "shared/assets/icons/theme-dark.svg";
+import { Button, ThemeButton } from "shared/ui/Button/Button";
 
 interface ThemeSwitcherProps {
   className?: string;
@@ -11,11 +14,12 @@ export const ThemeSwitcher: FC<ThemeSwitcherProps> = ({ className }) => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <button
+    <Button
+      theme={ThemeButton.CLEAR}
       className={classNames(cls.ThemeSwitcher, {}, [className])}
       onClick={toggleTheme}
     >
-      TOGGLE
-    </button>
+      {theme === Theme.DARK ? <DarkIcon /> : <LightIcon />}
+    </Button>
   );
 };
